71bc2c039d69eba6086b92cf96fd2c6ca57076bf
Joseph Bertram <jbertra3@nd.edu>
2025-06-01 17:45:13 -0400

    Implement server functionality

--- v0/server/python/server.py
+++ v0/server/python/server.py
@@ -6,6 +6,8 @@ PORT = 9999
 BUFFER_SIZE = 4096
 LOG_FILE = "server_log.txt"
 
+NUM_PACKET_TRAINS = 0
+
 AUTHORIZED_USERS = {
     "bertram": "fightingirish"
 }
@@ -22,36 +24,62 @@ def run_server():
 
     while True:
         data, addr = sock.recvfrom(BUFFER_SIZE)
-        request = json.loads(data.decode())
-        print(f"Received from {addr}: {data.deocde()}")
-
-        username = request["username"]
-        timestamp = request["timestamp"]
-        signature = request["signature"]
-        payload = request["payload"]
-
-        payload = {
-            "status": "OK",
-            "train_id": 42
-        }
-
-        timestamp = str(int(time.time()))
-
-        message = f"{timestamp}{json.dumps(payload, sort_keys=True)}".encode()
-
-        signature = 
-
-        if username not in AUTHORIZED_USERS:
-            print("[-] Unknown user")
-            continue
         
-        if not verify_hmac(AUTHORIZED_USERS[username], username, timestamp, payload, signature):
-            print("[-] Invalid signature")
-            continue
+        type_byte = data[0]
+        json_byte = data[1:]
         
-        sock.sendto("Authenticated", addr) 
-        print(f"[+] Authenticated request from {username}")
-
+        json_data = json.loads(json_byte.decode('utf-8'))
+        
+        if type_byte == 0x01:
+            print("Received config file from client!")
+            
+            username = json_data["username"]
+            timestamp = json_data["timestamp"]
+            signature = json_data["signature"]
+            payload = json_data["payload"]
+            
+            if username not in AUTHORIZED_USERS:
+                print("[-] Unknown user")
+                continue
+            
+            if not verify_hmac(AUTHORIZED_USERS[username], username, timestamp, payload, signature):
+                print("[-] Invalid signature")
+                continue
+            
+            print(f"[+] Authenticated request from {username}")
+            
+            NUM_PACKET_TRAINS = json_data["payload"]["count"]
+        
+        elif type_byte == 0x02:
+            print("Received pattern file from client!")
+            
+            for packet_train in range(1, NUM_PACKET_TRAINS + 1, 1):
+                sequence = json_data[f"{packet_train}"]
+                
+                num_packets = sequence["num_packets"]
+                packet_size = sequence["packets_size"]
+                local_gap = sequence["local_gap"] / 1000
+                global_gap = sequence["global_gap"] / 1000
+                
+                for packet in range(num_packets):
+                    payload = {
+                        "packet_train": packet_train,
+                        "packet_id": packet,
+                        "timestamp": time.time()
+                    }
+                    
+                    data = json.dumps(payload).encode()
+                    data += b' ' * max(0, packet_size - len(data))
+                    sock.sendto(data, addr)
+                    print(f"Sent packet {packet + 1} of packet train {packet_train} at time {timestamp}")
+                    
+                    with open("server_log.txt", "a") as log_file:
+                        log_file.write(f"Sent packet {packet + 1} of packet train {packet_train} at time {timestamp}\n")
+                    time.sleep(local_gap)
+            
+                time.sleep(global_gap)
+            
+        
 if __name__ == '__main__':
     run_server()
 
